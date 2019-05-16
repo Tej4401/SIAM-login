acae45023e191364389da11c7d21c6e2201cf66c
TEJ PRAKASH AGARWAL <49359890+Tej4401@users.noreply.github.com>
2019-05-16 13:26:55 +0530

    Update login.php

--- login.php
+++ login.php
@@ -16,24 +16,24 @@ session_start();
             <div class="row">
                 <div class="col-md-6 register">
                 <h3>SIGN UP:</h3>
-                    <form action="validation.php" method="POST">
+                    <form action="validation.php" method="POST" enctype="multipart/form-data">
                     <div class="form-group">
                         <label>NAME:&nbsp;</label><input type="text" class="form-control-sm" placeholder="name" name="name" id="name" required><br></div>
                         <div class="form-group"><label>USERNAME: &nbsp;</label><input type="text" class="form-control-sm" placeholder="username" name="username" id="username" required><br></div>
                         <div class="form-group"><label>PASSWORD: &nbsp;</label><input type="password" class="form-control-sm" placeholder="password" name="password" id="password" required><br></div>
-                        <div class="form-group"><label>EMAIL-ID: &nbsp;</label><input type="email" class="form-control-sm" placeholder="email-id" name="email" id="email"><br></div>
-                        <div class="form-group"><label>CONTACT NO: &nbsp;</label><input type="number" class="form-control-sm" placeholder="contact" name="contact" id="contact"><br></div>
+                        <div class="form-group"><label>EMAIL-ID: &nbsp;</label><input type="email" class="form-control-sm" placeholder="email-id" name="email" id="email" required><br></div>
+                        <div class="form-group"><label>CONTACT NO: &nbsp;</label><input type="number" class="form-control-sm" placeholder="contact" name="contact" id="contact" required><br></div>
                         <p>SELECT YOUR TEAM:</p>
-                        <div class="form-group"><select name="team" id="team">
+                        <div class="form-group"><select name="team" id="team" required>
                                                           <option value="collab">COLLABORATION AND OUTREACH</option>
                                                           <option value="research">RESEARCH</option>
                                                           <option value="marketing">MARKETING</option>
                                                         </select></div>
                         <p>CHOOSE YOUR POSITION: </p>
-                     <div> <input type="radio" name="pos" id="core" value="CORE-MEMBER">  <label><b>CORE-MEMBER</b></label>
+                     <div> <input type="radio" name="pos" id="core" value="CORE-MEMBER" required>  <label><b>CORE-MEMBER</b></label>
                        <input type="radio" name="pos" id="board" value="BOARD-MEMBER"> <label><b>BOARD MEMBER</b></label></div>
-                        <div class="form-group"><label>UPLOAD YOUR PHOTO: &nbsp;</label><input type="file" class="form-control-sm" name="photo" id="photo"></div>
-                        <input type="submit" class="btn btn-success button" value="signup">
+                        <div class="form-group"><label>UPLOAD YOUR PHOTO: &nbsp;</label><input type="file" class="form-control-sm" name="image" id="photo" required></div>
+                        <input type="submit" class="btn btn-success button" name="upload" value="signup">
                     </form>
                 </div>
                 <div class="col-md-6 login">
